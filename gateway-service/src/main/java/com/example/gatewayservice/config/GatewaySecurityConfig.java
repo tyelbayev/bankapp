11d46fad7aa1aef7ca303eefde37a85eeb62f984
turar <tyelbayev@gmail.com>
2025-08-25 22:50:12 +0500

    gateway

---
+++ gateway-service/src/main/java/com/example/gatewayservice/config/GatewaySecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.gatewayservice.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+@Configuration
+public class GatewaySecurityConfig {
+    @Bean
+    SecurityWebFilterChain springSecurity(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(ex -> ex
+                        .pathMatchers("/login**", "/assets/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .build();
+    }
+}
